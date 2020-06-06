2a392c9b5f02034399e0f505b8a7f30c6c198801
nt-76 <64823529+nt-76@users.noreply.github.com>
2020-06-07 01:25:34 +0530

    Update MavenPractice.java

--- src/main/java/MavenPractice.java
+++ src/main/java/MavenPractice.java
@@ -1,5 +1,25 @@
 public class MavenPractice {
     public static void main(String[] args) {
+        pipeline {
+    agent any
+    stages {
+        stage('Build Compilation') {
+            steps {
+                withmaven(maven : 'maven_3_6_3'){
+                sh '/Users/nthumu/Downloads/apache-maven-3.6.3/bin clean install'
+                }
+            }
+        }
+         stage('Build Test') {
+                    steps {
+                        withmaven(maven : 'maven_3_6_3'){
+                            sh 'Build Test'
+                            }
+                    }
+                }
+    }
+}
+
         System.out.println("mymaven test Sample");
     }
 }
