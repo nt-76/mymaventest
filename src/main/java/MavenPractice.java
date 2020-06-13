fd874b00f70d5d4482cce5bddeb74c89c66e7d31
nthumu <nthumu@nisum.com>
2020-06-13 23:35:33 +0530

    maven clean stmt change123456ab

--- src/main/java/MavenPractice.java
+++ src/main/java/MavenPractice.java
@@ -1,6 +1,6 @@
 public class MavenPractice {
     public static void main(String[] args) {
-        System.out.println("mymaven test Sample project added with no Jenkinsfile1256ytuy");
+        System.out.println("mymaven test Sample project added wvvvvvith no Jenkinsfile1256ytuy");
 }
     }
 
