2f108410a4377c2c4ac6697a6e116e2fdd2cb3bb
nt-76 <64823529+nt-76@users.noreply.github.com>
2020-06-14 22:58:46 +0530

    Update MavenPractice.java

--- src/main/java/MavenPractice.java
+++ src/main/java/MavenPractice.java
@@ -1,6 +1,6 @@
 public class MavenPractice {
     public static void main(String[] args) {
-        System.out.println("mymaven test Sample project added wvfffffvvvvhjhjhjjaaaaassssith no Jenkinsfile1256ytuy");
+        System.out.println("mymaven test Sample project added Test1");
 }
     }
 
