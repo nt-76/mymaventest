0b46c8071b95e4759dc09b3d79a9d6d0d3f6acf1
nt-76 <64823529+nt-76@users.noreply.github.com>
2020-06-14 23:30:59 +0530

    Update MavenPractice.java

--- src/main/java/MavenPractice.java
+++ src/main/java/MavenPractice.java
@@ -1,6 +1,7 @@
 public class MavenPractice {
     public static void main(String[] args) {
         System.out.println("mymaven test Sample project added Test11111");
+        System.out.println("mymaven test Sample project added Test22222");
 }
     }
 
